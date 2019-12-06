5a6f0eb2a2a1f9015b6e55e2417bcfe5988e3396
Samvel Mk <samuelmxitaryan@gmail.com>
2019-12-06 14:22:38 +0100

    added a function for filtering the results for the user at the end

--- static/main.js
+++ static/main.js
@@ -56,8 +56,42 @@ var app = new Vue({
           })
         })
       }
-  }
+  },
+  computed: {
+    
+    // this to compute the burnout scores based on Maslach scale. The burnoutScore is computed based the logic described at
+    // https://nam.edu/valid-reliable-survey-instruments-measure-burnout-well-work-related-dimensions/
+    // the burnout variable is a boolean value that is true or false based on the determined cut-off score on the given two dimensions
+    // Emotional exhaustion and Depersonalization. 
 
+    burnoutScore: function() {
+      let burnout = Boolean(false);
+      let sumEe = 0;
+      let sumDp = 0;
+
+      let EE = ['question_1', 'question_2', 'question_3', 'question_6',
+                'question_8', 'question_13', 'question_14', 'question_16', 'question_20'];
+
+      let DP = ['question_5', 'question_10', 'question_11', 'question_15', 'question_22'];
+
+      for( var el in this.answers.maslach ) {
+        if (EE.includes(el)){
+          if( this.answers.maslach.hasOwnProperty( el ) ) {
+            sumEe += parseInt( this.answers.maslach[el] );
+          } 
+        } else if (DP.includes(el)){
+          sumDp += parseInt( this.answers.maslach[el] )
+        }
+      }
+      if (sumEe >= 27 || sumDp >=10){
+        burnout = Boolean(true)
+        return burnout;
+      } else {
+        return burnout;
+      }
+    }
+   }
+  
 })
 
 function ChangeColor(tableRow, highLight)
